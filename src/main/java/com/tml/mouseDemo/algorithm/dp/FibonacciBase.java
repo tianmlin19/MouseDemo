8d0869933a38c78bfcd7aef727b7cf0abb2c3584
tianmaolin <tianmlin@126.com>
2021-07-16 08:29:53 +0800

    多数据源配置

---
+++ src/main/java/com/tml/mouseDemo/algorithm/dp/FibonacciBase.java
@@ -0,0 +1,65 @@
+package com.tml.mouseDemo.algorithm.dp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
+
+@Slf4j
+/**
+ * 斐波那契数列计算，采用传统的递归方法
+ * 递归方法的好处就是代码简单易懂，但弊端也显而易见，这里有大量的重复计算，导致算法的时间复杂度为2^n
+ */
+public class FibonacciBase {
+
+    public static void main(String[] args) {
+        /**
+         * 斐波那契数列
+         * 0 1 1 2 3 5 8 13
+         */
+        StopWatch sw = new StopWatch();
+
+        int num = 50;
+        sw.start("斐波那契数列计算" + num);
+        long fibonacciNum = getFibonacciNum(num);
+        log.info("计算斐波那契数列：{}返回结果：{}", num, fibonacciNum);
+        sw.stop();
+
+
+        num = 50;
+        sw.start("斐波那契数列计算" + num);
+        fibonacciNum = dpfibonacci(num);
+        log.info("计算斐波那契数列：{}返回结果：{}", num, fibonacciNum);
+        sw.stop();
+
+        System.out.println(sw.prettyPrint());
+    }
+
+    /**
+     * 普通的递归计算
+     * @param num
+     * @return
+     */
+    public static long getFibonacciNum(int num){
+        if (num < 3) {
+            return num-1;
+        }
+        return getFibonacciNum(num - 1) + getFibonacciNum(num - 2);
+    }
+
+    /**
+     * 采用自底向上的方式计算
+     * @param num 斐波那契数列的第几个数从1开始
+     *            0 1 1 2 3 5 8 13
+     * @return
+     */
+    public static long dpfibonacci(int num){
+        long[] dp = new long[num];
+        dp[1] = 1;
+        if (num < 3) {
+            return dp[num - 1];
+        }
+        for (int i = 2; i < num; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[num - 1];
+    }
+}
---
+++ src/main/java/com/tml/mouseDemo/io/bio/SocketServer.java
@@ -0,0 +1,31 @@
+package com.tml.mouseDemo.io.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(9000);
+        while (true) {
+            System.out.println("等待连接-----------");
+            Socket socket = serverSocket.accept();
+            System.out.println("有客户端连接了--------");
+            handler(socket);
+        }
+
+    }
+
+    private static void handler(Socket clientSocket) throws IOException {
+        byte[] contents = new byte[1024];
+        System.out.println("准备read~");
+        int read = clientSocket.getInputStream().read(contents);
+        System.out.println("read end~");
+
+        if (read != -1) {
+            System.out.println("接收到客户端的数据：" + new String(contents, 0, read));
+        }
+
+    }
+}
