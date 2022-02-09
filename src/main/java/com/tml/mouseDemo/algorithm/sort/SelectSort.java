7ae4588ff595b57846613bd23b1d549e4ae38c7e
tianmaolin <tianmlin@126.com>
2022-02-09 10:33:33 +0800

    选择排序

---
+++ src/main/java/com/tml/mouseDemo/algorithm/sort/SelectSort.java
@@ -0,0 +1,42 @@
+package com.tml.mouseDemo.algorithm.sort;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
+/**
+ * 选择排序
+ * 时间复杂度为O（n2）
+ */
+@Slf4j
+public class SelectSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 7, 1, 8, 3, 9, 0, 4, 2};
+
+
+        for (int i = 0; i < arr.length; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minPos]) {
+                    minPos = j;
+                }
+            }
+
+            //交换i和minpos的位置
+            int tmp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = tmp;
+
+
+            System.out.println("第" + i + "次排序结果：" + Arrays.toString(arr));
+        }
+
+
+        System.out.println("____________________________");
+        System.out.println("排序之后的结果为：" + Arrays.toString(arr));
+
+    }
+
+}
