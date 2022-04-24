e6329d90356d01cd6ff2d441953552116c30c1dc
tianmaolin <tianmlin@126.com>
2022-04-24 08:57:00 +0800

    选择排序

---
+++ src/main/java/com/tml/mouseDemo/algorithm/leetcode/LinkListAdd.java
@@ -0,0 +1,62 @@
+package com.tml.mouseDemo.algorithm.leetcode;
+
+/**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * <p>
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * <p>
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LinkListAdd {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class ListNode {
+        public int val;
+
+        public ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
+}
--- src/main/java/com/tml/mouseDemo/algorithm/leetcode/ToSum.java
+++ src/main/java/com/tml/mouseDemo/algorithm/leetcode/ToSum.java
@@ -1,13 +1,75 @@
 package com.tml.mouseDemo.algorithm.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 两数之和
- *
+ * <p>
  * 给定 nums = [2, 7, 11, 15], target = 9
- *
+ * <p>
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
- *
+ * <p>
  * 所以返回[0, 1]
  */
 public class ToSum {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{2, 7, 11, 15};
+        int target = 26;
+
+        int[] twoSum = getTwoSumLoop(array, target);
+
+        System.out.println(Arrays.toString(twoSum));
+
+
+        int[] twoSumMap = getTwoSumMap(array, target);
+        System.out.println(Arrays.toString(twoSumMap));
+
+    }
+
+
+    /**
+     * 两层循环 此时的时间复杂度是O（n^2）
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] getTwoSumLoop(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 此时的时间复杂度是O(n)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] getTwoSumMap(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && (map.get(target - nums[i]) != i)) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+        }
+
+        return null;
+    }
 }
